2a0f7c345dfa4880401d07f57ba54b0709de3f88
Malka <malka.nessy@gmail.com>
2017-10-03 21:12:10 +0300

    ready for nodejs

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,32 +9,8 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }]).
 controller("View1Ctrl", ['$scope', function($scope) {
-    $scope.delete = function(data) {
-        data.nodes = [];
-    };
-    $scope.add = function(data) {
-    		if(data.id in $scope.originalDataDict){
-        	//console.log("found");
-          //console.log($scope.originalDataDict[data.id]);
-          var nodes = $scope.getNodes($scope.originalDataDict[data.id]);
-          for(var n in nodes){
-          	data.nodes.push(nodes[n]);
-          }
-        }else{
-        	console.log("not found");
-        }
-    };
-    $scope.click = function(data){
-    	if(data.nodes.length != 0){
-      	$scope.delete(data);
-      }else{
-      	$scope.add(data);
-      }
-    };
-    $scope.originalDataDict = {
-    
-    };
-    $scope.originalData = [
+	
+	$scope.originalData = [
         {
             id:1,
             name: "Friends",
@@ -62,10 +38,31 @@ controller("View1Ctrl", ['$scope', function($scope) {
         },
         {id: 8, name: "Ori", type: "Contact"},
     ];
+	
+	$scope.originalDataDict = {
+    
+    };
+	
+	$scope.selectedId = -1;
+	
+    $scope.getNodes = function(root){
+    	//console.log("getNodes");
+    	//console.log(item);
+		var col = [];
+		for(var itemKey in root){
+			var item = root[itemKey];
+			var i1 = {name:item.name,id:item.id, type:item.type, nodes:[]};
+			col.push(i1);
+		}
+		console.log("getNodes col = " + col);
+    	return col;
+     
+    };
+	
     $scope.parseDictItem = function(item){
     	//console.log("parseDictItem");
-     console.log("parseDictItem item = " +item);
-      if(item.type=="Group"){
+		console.log("parseDictItem item = " +item);
+		if(item.type=="Group"){
     		$scope.originalDataDict[item.id] = item.contacts;
         for(var c in item.contacts)
         {
@@ -74,29 +71,44 @@ controller("View1Ctrl", ['$scope', function($scope) {
        
       }
     };
-    $scope.getNodes = function(root){
-    	//console.log("getNodes");
-    	//console.log(item);
-    	var col = [];
-      for(var itemKey in root){
-      	var item = root[itemKey];
-        var i1 = {name:item.name,id:item.id, type:item.type, nodes:[]};
-      	col.push(i1);
-      }
-      console.log("getNodes col = " + col);
-    	return col;
-     
-    };
-    
+	
+   
     $scope.parseRoot = function(){
-      //console.log("parse root started");
+		//console.log("parse root started");
     	var col =  $scope.getNodes($scope.originalData);      
-      for(var itemKey in $scope.originalData){
-      	var item = $scope.originalData[itemKey];
-        $scope.parseDictItem(item);
-      }
-      console.log("parseRoot col = " + col);
-      return col;
+		for(var itemKey in $scope.originalData){
+			var item = $scope.originalData[itemKey];
+			$scope.parseDictItem(item);
+		}
+		console.log("parseRoot col = " + col);
+		return col;
     };
     $scope.tree = $scope.parseRoot();
+	
+	$scope.delete = function(data) {
+        data.nodes = [];
+    };
+	
+    $scope.add = function(data) {
+		if(data.id in $scope.originalDataDict){
+		//console.log("found");
+		//console.log($scope.originalDataDict[data.id]);
+			var nodes = $scope.getNodes($scope.originalDataDict[data.id]);
+			for(var n in nodes){
+				data.nodes.push(nodes[n]);
+			}
+        }else{
+        	console.log("not found");
+        }
+    };
+	
+    $scope.click = function(data){
+      $scope.selectedId = data.id;
+      if(data.nodes.length != 0){
+      	$scope.delete(data);
+      }else{
+      	$scope.add(data);
+      }
+    };
+	
 }]);
